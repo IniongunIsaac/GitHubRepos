6a5f3d4c40f004aa7ad606feba19cf0e917e65f9
Isaac Iniongun <iniongunisaac@gmail.com>
2023-11-24 20:26:22 +0100

    Initial Commit

---
+++ GitHubRepos/GitHubReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubReposApp.swift
+//  GitHubRepos
+//
+//  Created by Isaac Iniongun on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
